8579c8bb7a18fddcef35582ba1126f1b2766a6ca
gaya3rk <60214951+gaya3rk@users.noreply.github.com>
2020-03-17 10:57:14 +0530

    Create product.java

---
+++ product.java
@@ -0,0 +1,28 @@
+
+public class product {
+	private int productId;
+	private String proName;
+	private double price;
+	 
+	public void setProductId(int productId)
+	{
+		this.productId=productId;
+	}
+	public int getProductId()
+	{
+		return productId;
+	}
+	public String getProName() {
+		return proName;
+	}
+	public void setProName(String proName) {
+		this.proName = proName;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+}
